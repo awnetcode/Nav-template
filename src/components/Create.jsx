c8bc71b6c6351c8c16a04518fb6e3d54762073c5
awnetcode <155958835+awnetcode@users.noreply.github.com>
2024-08-13 10:13:43 +0200

    13.08

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -3,18 +3,25 @@ const Create = () => {
         <>
         <div className="text">
         <h3>Tworzenie strony w React + Vite:</h3>
-        <h4 className="steps">
-            <p className="step">W terminalu wchodzimy do folderu z projektem </p>
+        <h4 className="steps"></h4>
+            <p className="step">W terminalu wchodzimy do folderu w którym chcemy umieścić projekt. </p>
+            <div className="code-block">
             <code className="code-insert">cd Nazwa_folderu</code>
+            </div>
             <p className="step">Projekt tworzymy komendą </p>
+            <div className="code-block">
             <code className="code-insert">create vite@latest</code>
-            <p className="step">Kreator zada nam kilka pytań: o nazwę projektu, framework i język programowania. Po zakończeniu będziemy mieli folder o nazwie takiej jaką podaliśmy przy tworzeniu, a w środku podstawową strukturę plików.</p>
-            <p>Przechodzimy do tego folderu za pomocą terminala i instalujemy npm.</p>
+            </div>
+            <p className="step">Kreator zada nam kilka pytań: o nazwę projektu, framework i język programowania. Po zakończeniu będziemy mieli folder o nazwie takiej jaką podaliśmy przy tworzeniu, a w środku podstawową strukturę plików.
+            Przechodzimy do tego folderu za pomocą terminala i instalujemy npm.</p> 
+            <div className="code-block">
             <code className="code-insert">npm install</code>
-            <p>Ostatnim krokiem jest uruchomienie projektu w przeglądarce.</p>
+            </div>
+            <p className="step">Ostatnim krokiem jest uruchomienie projektu w przeglądarce.</p>
+            <div className="code-block">
             <code className="code-insert">npm run dev</code>
-            <p>Uruchamia nam się serwer na localhost na porcie 5173. Należy otworzyć go w przeglądarce przytrzymując ctrl i klikajac myszką w link w terminalu. Od tej pory każde zapisanie zmian w pliku spowoduje aktualizację tego co widać w przeglądarce.</p>
-        </h4>
+            </div>
+            <p className="step">Uruchamia nam się serwer na localhost na porcie 5173. Należy otworzyć go w przeglądarce przytrzymując ctrl i klikajac myszką w link w terminalu. Od tej pory każde zapisanie zmian w pliku spowoduje aktualizację tego co widać w przeglądarce.</p>
         </div>
      
         </>
